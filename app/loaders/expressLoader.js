42e49a51b12f58df80dbcda576493ddb234ff64d
ajaykumarnarangali <www.ajayanarangali@gmail.com>
2025-07-25 23:35:14 +0530

    nodemon updation and swagger implementation

--- app/loaders/expressLoader.js
+++ app/loaders/expressLoader.js
@@ -1,9 +1,17 @@
 const morgan = require('morgan');
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 const { routerLoader } = require('./routerLoader');
 const cookieParser = require('cookie-parser');
+
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const swaggerDocument = YAML.load(
+    path.join(__dirname, "..", "swagger", "swagger.yaml")
+);
 
 const {
     bodyParserHandler,
@@ -19,6 +27,7 @@ async function expressLoader(app) {
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(bodyParserHandler);
+    app.use('/swagger-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
     routerLoader(app);
 
